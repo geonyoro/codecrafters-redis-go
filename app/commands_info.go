747ae8ff995f23036b68c1898a55a3cd405e5e6c
George Nyoro <george@skylinedesign.co.ke>
2026-02-26 09:28:24 +0300

    codecrafters submit [skip ci]

--- app/commands_info.go
+++ app/commands_info.go
@@ -11,12 +11,12 @@ func Info(ctx *Context, cmd Command) ReturnValue {
 		RBulkString, fmt.Sprintf(`
 role:%s
 connected_slaves:0
-master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb
-master_repl_offset:0
+master_replid:%s
+master_repl_offset:%d
 second_repl_offset:-1
 repl_backlog_active:0
 repl_backlog_size:1048576
 repl_backlog_first_byte_offset:0
-repl_backlog_histlen:`, role),
+repl_backlog_histlen:`, role, ctx.State.Settings.MasterReplId, ctx.State.Settings.MasterReplOffset),
 	}
 }
